a7ba80df0e0aa4c5828b71dc84873e55f394f8ad
defaois2 <shahin.defaoitehoushidari2@mail.dcu.ie>
2023-08-03 23:25:52 +0100

    small changes, debug true

--- spelling_web_app/settings.py
+++ spelling_web_app/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#1^bh8qieanp8_)dk9ns!2gt-!cltgwznf_50szd9&!cmujj(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-DEVELOPMENT_MODE= True
+DEBUG = True
 ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
